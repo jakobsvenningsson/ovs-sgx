fb2978db9f642cbd296d76856eef32143373c635
jakob <jaksve@kth.se>
2019-05-08 14:36:45 +0200

    Added complete versions of list.c and list.h

--- OFTonSGX/trusted/lib/list.c
+++ OFTonSGX/trusted/lib/list.c
@@ -1,10 +1,19 @@
 /*
- * list.c
+ * Copyright (c) 2008, 2009, 2010, 2011, 2012, 2013 Nicira, Inc.
  *
- *  Created on: Apr 23, 2018
- *      Author: jorge
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
+#include <config.h>
 #include "list.h"
 
 /* Initializes 'list' as an empty list. */
@@ -14,19 +23,14 @@ list_init(struct list *list)
     list->next = list->prev = list;
 }
 
-
-/* Removes 'elem' from its list and returns the element that followed it.
-   Undefined behavior if 'elem' is not in a list. */
-struct list *
-list_remove(struct list *elem)
+/* Initializes 'list' with pointers that will (probably) cause segfaults if
+ * dereferenced and, better yet, show up clearly in a debugger. */
+void
+list_poison(struct list *list)
 {
-    elem->prev->next = elem->next;
-    elem->next->prev = elem->prev;
-    return elem->next;
+    memset(list, 0xcc, sizeof *list);
 }
 
-
-//lines 34
 /* Inserts 'elem' just before 'before'. */
 void
 list_insert(struct list *before, struct list *elem)
@@ -57,13 +61,15 @@ list_splice(struct list *before, struct list *first, struct list *last)
     before->prev->next = first;
     before->prev = last;
 }
-//lines 64
 
+/* Inserts 'elem' at the beginning of 'list', so that it becomes the front in
+   'list'. */
+void
+list_push_front(struct list *list, struct list *elem)
+{
+    list_insert(list->next, elem);
+}
 
-
-
-
-//lines 73
 /* Inserts 'elem' at the end of 'list', so that it becomes the back in
  * 'list'. */
 void
@@ -82,9 +88,85 @@ list_replace(struct list *element, const struct list *position)
     element->prev = position->prev;
     element->prev->next = element;
 }
-//lines 90
 
-//lines 170
+/* Adjusts pointers around 'list' to compensate for 'list' having been moved
+ * around in memory (e.g. as a consequence of realloc()).
+ *
+ * This always works if 'list' is a member of a list, or if 'list' is the head
+ * of a non-empty list.  It fails badly, however, if 'list' is the head of an
+ * empty list; just use list_init() in that case. */
+void
+list_moved(struct list *list)
+{
+    list->prev->next = list->next->prev = list;
+}
+
+/* Removes 'elem' from its list and returns the element that followed it.
+   Undefined behavior if 'elem' is not in a list. */
+struct list *
+list_remove(struct list *elem)
+{
+    elem->prev->next = elem->next;
+    elem->next->prev = elem->prev;
+    return elem->next;
+}
+
+/* Removes the front element from 'list' and returns it.  Undefined behavior if
+   'list' is empty before removal. */
+struct list *
+list_pop_front(struct list *list)
+{
+    struct list *front = list->next;
+    list_remove(front);
+    return front;
+}
+
+/* Removes the back element from 'list' and returns it.
+   Undefined behavior if 'list' is empty before removal. */
+struct list *
+list_pop_back(struct list *list)
+{
+    struct list *back = list->prev;
+    list_remove(back);
+    return back;
+}
+
+/* Returns the front element in 'list_'.
+   Undefined behavior if 'list_' is empty. */
+struct list *
+list_front(const struct list *list_)
+{
+    struct list *list = CONST_CAST(struct list *, list_);
+
+    ovs_assert(!list_is_empty(list));
+    return list->next;
+}
+
+/* Returns the back element in 'list_'.
+   Undefined behavior if 'list_' is empty. */
+struct list *
+list_back(const struct list *list_)
+{
+    struct list *list = CONST_CAST(struct list *, list_);
+
+    ovs_assert(!list_is_empty(list));
+    return list->prev;
+}
+
+/* Returns the number of elements in 'list'.
+   Runs in O(n) in the number of elements. */
+size_t
+list_size(const struct list *list)
+{
+    const struct list *e;
+    size_t cnt = 0;
+
+    for (e = list->next; e != list; e = e->next) {
+        cnt++;
+    }
+    return cnt;
+}
+
 /* Returns true if 'list' is empty, false otherwise. */
 bool
 list_is_empty(const struct list *list)
@@ -92,9 +174,16 @@ list_is_empty(const struct list *list)
     return list->next == list;
 }
 
+/* Returns true if 'list' has exactly 1 element, false otherwise. */
+bool
+list_is_singleton(const struct list *list)
+{
+    return list_is_short(list) && !list_is_empty(list);
+}
 
-
-
-
-
-
+/* Returns true if 'list' has 0 or 1 elements, false otherwise. */
+bool
+list_is_short(const struct list *list)
+{
+    return list->next == list->prev;
+}
--- OFTonSGX/trusted/lib/list.h
+++ OFTonSGX/trusted/lib/list.h
@@ -1,12 +1,20 @@
 /*
- * list.h
+ * Copyright (c) 2008, 2009, 2010, 2011 Nicira, Inc.
  *
- *  Created on: Apr 23, 2018
- *      Author: jorge
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
-#ifndef ENCLAVE_MYENCLAVE_TRUSTED_LIB_LIST_H_
-#define ENCLAVE_MYENCLAVE_TRUSTED_LIB_LIST_H_
+#ifndef LIST_H
+#define LIST_H 1
 
 /* Doubly linked list. */
 
@@ -15,29 +23,39 @@
 #include "util.h"
 #include "common.h"
 
-
 /* Doubly linked list head or element. */
-/*struct list {
-    struct list *prev;
-    struct list *next;
-};*/
+//struct list {
+//    struct list *prev;     /* Previous list element. */
+//    struct list *next;     /* Next list element. */
+//};
 
 #define LIST_INITIALIZER(LIST) { LIST, LIST }
 
-
 void list_init(struct list *);
+void list_poison(struct list *);
 
 /* List insertion. */
 void list_insert(struct list *, struct list *);
 void list_splice(struct list *before, struct list *first, struct list *last);
-void list_replace(struct list *, const struct list *);
+void list_push_front(struct list *, struct list *);
 void list_push_back(struct list *, struct list *);
+void list_replace(struct list *, const struct list *);
+void list_moved(struct list *);
 
 /* List removal. */
 struct list *list_remove(struct list *);
+struct list *list_pop_front(struct list *);
+struct list *list_pop_back(struct list *);
+
+/* List elements. */
+struct list *list_front(const struct list *);
+struct list *list_back(const struct list *);
 
 /* List properties. */
+size_t list_size(const struct list *);
 bool list_is_empty(const struct list *);
+bool list_is_singleton(const struct list *);
+bool list_is_short(const struct list *);
 
 #define LIST_FOR_EACH(ITER, MEMBER, LIST)                               \
     for (ASSIGN_CONTAINER(ITER, (LIST)->next, MEMBER);                  \
@@ -62,10 +80,4 @@ bool list_is_empty(const struct list *);
           : 0);                                                 \
          (ITER) = (NEXT))
 
-
-
-
-
-
-
-#endif /* ENCLAVE_MYENCLAVE_TRUSTED_LIB_LIST_H_ */
+#endif /* list.h */
