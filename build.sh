4088b2087fcd49a752b4aa69ea58cab526fbd47f
jakob <jaksve@kth.se>
2019-08-06 13:50:16 +0200

    updated build.sh

--- build.sh
+++ build.sh
@@ -5,22 +5,22 @@ FLAGS=$1
 source /opt/intel/sgxsdk/environment
 echo "%%%%%%%%%%%%%%%%%%%%%%%%% BUILDING HOTCALL BUNDLER TRUSTED"
 make -C hotcall_bundler/src/trusted clean
-make -C hotcall_bundler/src/trusted
+make -C hotcall_bundler/src/trusted SGX_MODE=HW SGX_PRERELEASE=1 SGX_DEBUG=0 LFLAGS="$FLAGS"
 
 echo "%%%%%%%%%%%%%%%%%%%%%%%%% BUILDING HOTCALL BUNDLER UNTRUSTED"
 make -C hotcall_bundler/src/untrusted clean
-make -C hotcall_bundler/src/untrusted
+make -C hotcall_bundler/src/untrusted SGX_MODE=HW SGX_PRERELEASE=1 SGX_DEBUG=0 LFLAGS="$FLAGS"
 
 echo "%%%%%%%%%%%%%%%%%%%%%%%%% BUILDING OFTonSGX"
 make -C OFTonSGX/ clean
 make -C OFTonSGX/ SGX_MODE=HW SGX_PRERELEASE=1 SGX_DEBUG=0 LFLAGS="$FLAGS"
 cp OFTonSGX/enclave.signed.so ./ovs
 
-echo "%%%%%%%%%%%%%%%%%%%%%%%%% BUILDING TLSonSGX"
-make -C TLSonSGX/ clean
-make -C TLSonSGX/ SGX_MODE=HW SGX_PRERELEASE=1 SGX_DEBUG=0 LFLAGS="$FLAGS"
-make -C TLSonSGX/ wrapper_library
-cp TLSonSGX/tls_enclave.signed.so ./ovs
+#echo "%%%%%%%%%%%%%%%%%%%%%%%%% BUILDING TLSonSGX"
+#make -C TLSonSGX/ clean
+#make -C TLSonSGX/ SGX_MODE=HW SGX_PRERELEASE=1 SGX_DEBUG=0 LFLAGS="$FLAGS"
+#make -C TLSonSGX/ wrapper_library
+#cp TLSonSGX/tls_enclave.signed.so ./ovs
 
 echo "%%%%%%%%%%%%%%%%%%%%%%%%% BUILDING OvS"
 cd ovs
@@ -31,6 +31,7 @@ cd ovs
                            -I${ROOT_FOLDER}/OFTonSGX/include \
                            -I${ROOT_FOLDER}/benchmark/include \
                            -I${ROOT_FOLDER}/hotcall_bundler/include \
+                           -I${ROOT_FOLDER}/hotcall_bundler/src/lib \
                            -I${ROOT_FOLDER}/hotcall_bundler/src/untrusted \
                            -I/opt/intel/sgxsdk/include \
                            -I${ROOT_FOLDER}/TLSonSGX/mbedtls/include" \
@@ -39,7 +40,7 @@ cd ovs
                          -L$ROOT_FOLDER/OFTonSGX \
                          -L${ROOT_FOLDER}/TLSonSGX/mbedtls/library \
                          -L$ROOT_FOLDER/TLSonSGX" \
-            	LIBS="  -lOFTonSGX -lTLSonSGX -lhotcall_bundler_untrusted -lmbedtls -lmbedx509 -lmbedcrypto -lpthread -lstdc++ "
+            	LIBS="  -lOFTonSGX -lhotcall_bundler_untrusted -lpthread -lstdc++ "
 
 
 make clean
